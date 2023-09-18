9fcbf3936c11e7df71ab207515821af71046855a
david temitope <temi246topemicheal@gmail.com>
2023-09-18 02:00:36 +0100

    fix: on empty page component

---
+++ app/HomeList/[homeID]/page.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import HouseList from "./components/HouseList";
+import SearchBars from "./components/SearchBars";
+
+type Props = {};
+
+const page = (props: Props) => {
+  return (
+    <section className="">
+      home details
+    </section>
+  );
+};
+
+export default page;
